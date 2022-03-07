94cbcd4475262f2a0bc59eafe6a7aef4751f1738
Kjetil <kjetil@muybridge.xyz>
2022-03-07 15:09:28 +0100

    Fix a bug in the ptx-kernel calling convention where structs was passed indirectly
    Structs being passed indirectly is suprpising and have a high chance not to work as the device and host usually do not share memory.

--- compiler/rustc_target/src/abi/call/nvptx64.rs
+++ compiler/rustc_target/src/abi/call/nvptx64.rs
@@ -1,7 +1,7 @@
 // Reference: PTX Writer's Guide to Interoperability
 // https://docs.nvidia.com/cuda/ptx-writers-guide-to-interoperability
 
-use crate::abi::call::{ArgAbi, FnAbi};
+use crate::abi::call::{ArgAbi, Conv, FnAbi};
 
 fn classify_ret<Ty>(ret: &mut ArgAbi<'_, Ty>) {
     if ret.layout.is_aggregate() && ret.layout.size.bits() > 64 {
@@ -19,15 +19,35 @@ fn classify_arg<Ty>(arg: &mut ArgAbi<'_, Ty>) {
     }
 }
 
+fn classify_arg_ptx_kernel<Ty>(arg: &mut ArgAbi<'_, Ty>) {
+    if arg.layout.is_unsized() {
+        arg.make_indirect();
+    } else if arg.layout.is_aggregate() && arg.layout.size.bits() < 64 {
+        arg.extend_integer_width_to(64);
+    }
+}
+
 pub fn compute_abi_info<Ty>(fn_abi: &mut FnAbi<'_, Ty>) {
     if !fn_abi.ret.is_ignore() {
         classify_ret(&mut fn_abi.ret);
     }
 
-    for arg in &mut fn_abi.args {
-        if arg.is_ignore() {
-            continue;
+    match fn_abi.conv {
+        Conv::PtxKernel => {
+            for arg in &mut fn_abi.args {
+                if arg.is_ignore() {
+                    continue;
+                }
+                classify_arg_ptx_kernel(arg);
+            }
         }
-        classify_arg(arg);
+        _ => {
+            for arg in &mut fn_abi.args {
+                if arg.is_ignore() {
+                    continue;
+                }
+                classify_arg(arg);
+            }       
+        }
     }
 }
